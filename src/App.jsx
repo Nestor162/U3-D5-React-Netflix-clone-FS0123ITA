b070034588fc7641d48222363486ce162f41741a
Nestor162 <nestorcicardini@gmail.com>
2023-03-31 12:48:25 +0200

    Componenti per sezione filtri

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import NetflixNav from "./components/NetflixNav";
+import MainContent from "./components/MainContent";
 
 function App() {
   return (
     <main className="App">
       <NetflixNav />
+      <MainContent />
     </main>
   );
 }
---
+++ src/components/MainContent.jsx
@@ -0,0 +1,5 @@
+import Filters from "./Filters";
+
+const MainContent = () => <Filters />;
+
+export default MainContent;
