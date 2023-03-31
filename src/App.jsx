3324ac4ac7680924349d5bab54bc54cf7e9a8ab3
Nestor162 <nestorcicardini@gmail.com>
2023-03-31 18:07:24 +0200

    Implementata pagina settings

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import "./App.css";
 import NetflixNav from "./components/NetflixNav";
 import MainContent from "./components/MainContent";
 import NetflixFooter from "./components/NetflixFooter";
+/* import SettingsPage from "./components/settingsPage"; */
 
 function App() {
   return (
     <main className="App">
       <NetflixNav />
       <MainContent />
+      {/*   <SettingsPage /> */}
       <NetflixFooter />
     </main>
   );
