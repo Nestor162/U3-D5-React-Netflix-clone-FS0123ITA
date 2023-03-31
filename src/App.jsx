94dd67e162e7f8f39ce2797430026aeac692c7f6
Nestor162 <nestorcicardini@gmail.com>
2023-03-31 16:41:39 +0200

    Implementazione iniziale footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import NetflixNav from "./components/NetflixNav";
 import MainContent from "./components/MainContent";
+import NetflixFooter from "./components/NetflixFooter";
 
 function App() {
   return (
     <main className="App">
       <NetflixNav />
       <MainContent />
+      <NetflixFooter />
     </main>
   );
 }
---
+++ src/components/NetflixFooter.jsx
@@ -0,0 +1,101 @@
+const NetflixFooter = () => (
+  <footer className="footer-container mt-big">
+    <div className="ms-2rem mb-2">
+      <i className="bi bi-facebook fs-4 me-3 text-white-50" />
+      <i className="bi bi-instagram fs-4 me-3 text-white-50" />
+      <i className="bi bi-twitter fs-4 me-3 text-white-50" />
+      <i className="bi bi-youtube fs-4 me-3 text-white-50" />
+    </div>
+    <div className="row">
+      <div className="col-3">
+        <ul>
+          <li className="lh-lg">
+            <a className="text-white-50" href="#">
+              Audio and Subtitles
+            </a>
+          </li>
+          <li className="lh-lg">
+            <a className="text-white-50" href="#">
+              Media Center
+            </a>
+          </li>
+          <li className="lh-lg">
+            <a className="text-white-50" href="#">
+              Privacy
+            </a>
+          </li>
+          <li className="lh-lg">
+            <a className="text-white-50" href="#">
+              Contact us
+            </a>
+          </li>
+          <li className="lh-lg">
+            <button type="button" className="btn btn-outline-secondary btn-sm mt-4 rounded-0">
+              Service Code
+            </button>
+          </li>
+        </ul>
+      </div>
+      <div className="col-3">
+        <ul>
+          <li>
+            <a className="text-white-50 lh-lg" href="#">
+              Audio Description
+            </a>
+          </li>
+          <li>
+            <a className="text-white-50 lh-lg" href="#">
+              Investor relations
+            </a>
+          </li>
+          <li>
+            <a className="text-white-50 lh-lg" href="#">
+              Legal notices
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div className="col-3">
+        <ul>
+          <li>
+            <a className="text-white-50 lh-lg" href="#">
+              Help Center
+            </a>
+          </li>
+          <li>
+            <a className="text-white-50 lh-lg" href="#">
+              Jobs
+            </a>
+          </li>
+          <li>
+            <a className="text-white-50 lh-lg" href="#">
+              Cookie Preferences
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div className="col-3">
+        <ul>
+          <li>
+            <a className="text-white-50 lh-lg" href="#">
+              Gift Cards
+            </a>
+          </li>
+          <li>
+            <a className="text-white-50 lh-lg" href="#">
+              Term of Use
+            </a>
+          </li>
+          <li>
+            <a className="text-white-50 lh-lg" href="#">
+              Corporate Information
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <p className="text-white-50 small ps-2rem">© 1997-2019 Netflix, inc. (i.OdOOfcda2fdt9cOde)</p>
+  </footer>
+);
+
+export default NetflixFooter;
