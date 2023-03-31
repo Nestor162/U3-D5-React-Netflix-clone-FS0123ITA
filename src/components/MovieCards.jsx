7b3c0834fc1292e5545b2cde7db92c2f16a0acfb
Nestor162 <nestorcicardini@gmail.com>
2023-03-31 15:11:30 +0200

    Visualizzazione immagini dinamiche

---
+++ src/components/MovieCards.jsx
@@ -0,0 +1,47 @@
+import { Component } from "react";
+import { Row } from "react-bootstrap";
+
+class MovieCards extends Component {
+  state = {
+    movies: [],
+    error: false,
+    errorMsg: "",
+    isLoading: true
+  };
+
+  fetchMovies = query => {
+    fetch(`https://www.omdbapi.com/?apikey=69f0bbd5&${query}`)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error("C'e stato un errore nella richiesta al server");
+        }
+        return response.json();
+      })
+      .then(data => {
+        this.setState({ isLoading: false, movies: data.Search });
+        console.log(data.Search);
+      })
+      .catch(error => this.setState({ errorMsg: error.message, error: true, isLoading: false }));
+  };
+
+  componentDidMount() {
+    this.fetchMovies("s=one%20piece");
+    /*     this.fetchMovies("s=Gintama&type=Series"); */
+  }
+
+  render() {
+    return (
+      <Row>
+        {this.state.movies.map((movie, i) => (
+          <div className="col-xl-2 col-md-4 pb-2 col-sm-6 px-1" key={i}>
+            <div className="ImgContainer">
+              <img className="img-fluid ImgCard" src={movie.Poster} alt={movie.title} />
+            </div>
+          </div>
+        ))}
+      </Row>
+    );
+  }
+}
+
+export default MovieCards;
